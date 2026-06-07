68a18f612558be8e3d7f8d9615839a7008b59664
zhenshiz <2238446247@qq.com>
2026-06-07 22:30:33 +0800

    v1.1.0

--- src/assets/info/mod.js
+++ src/assets/info/mod.js
@@ -79,7 +79,7 @@ export const modList = () => {
         }
       ],
       moreUtil: EMPTY_MORE_UTIL,
-      modVersion: '1.1.6'
+      modVersion: '1.1.7'
     },
     {
       lang: 'ViScriptRecipe',
