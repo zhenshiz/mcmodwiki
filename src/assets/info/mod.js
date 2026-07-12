15b36d0e6905e6693205db5e9fff6dbe7c7ccaeb
zhenshiz <2238446247@qq.com>
2026-07-12 09:56:53 +0800

    v1.1.0

--- src/assets/info/mod.js
+++ src/assets/info/mod.js
@@ -101,7 +101,7 @@ export const modList = () => {
         }
       ],
       moreUtil: EMPTY_MORE_UTIL,
-      modVersion: '1.0.3 beta'
+      modVersion: '1.0.6 beta'
     },
     {
       lang: 'ViScriptQuests',
